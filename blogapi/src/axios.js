ed3e56fb975ea155115e63b44ce2f0d247435c4e
dhirajssh <dhirajssh@gmail.com>
2021-09-03 15:05:54 +0530

    axios intercept

--- blogapi/src/axios.js
+++ blogapi/src/axios.js
@@ -1,39 +1,99 @@
 import axios from 'axios';
 
+/*
+The axios.create() function creates a new Axios instance. When you require('axios'), you get back an the default Axios instance. The reason why you would create an instance is to set custom defaults for your application.
+
+Axios interceptors are functions that Axios calls for every request. You can use interceptors to transform the request before Axios sends it, or transform the response before Axios returns the response to your code.
+
+You can think of interceptors as Axios' equivalent to middleware in Express or Mongoose.
+
+The atob() method decodes a base-64 encoded string.
+This method decodes a string of data which has been encoded by the btoa() method.
+*/
+
 const baseURL = 'http://127.0.0.1:8000/api/';
 
 const axiosInstance = axios.create({
-  baseURL: baseURL,
-  timeout: 5000,
-  headers: {
-    Authorization: localStorage.getItem('access_token')
-      ? 'JWT ' + localStorage.getItem('access_token')
-      : null,
-    'Content-Type': 'application/json',
-    accept: 'application/json',
-  },
+	baseURL: baseURL,
+	timeout: 5000,
+	headers: {
+		Authorization: localStorage.getItem('access_token')
+			? 'JWT ' + localStorage.getItem('access_token')
+			: null,
+		'Content-Type': 'application/json',
+		accept: 'application/json',
+	}, 
 });
 
 axiosInstance.interceptors.response.use(
-  (response) => {
-    return response;
-  },
-  async function (error) {
-    const originalRequest = error.config;
+	(response) => {
+		return response;
+	},
+	async function (error) {
+		const originalRequest = error.config;
+    console.log(error.config);
 
-    if (typeof error.response === 'undefined') {
-      alert(
-        'A server/network error ocurred. ' +
-        'Looks like CORS might be the problem. ' +
-        'Sorry about this - we will get it fixed shortly.'
-      );
-      return Promise.reject(error);
-    }
-    if (error.response.status === 401 && originalRequest.url === baseURL + 'token/refresh/') {
-      window.location.href = '/login/';
-      return Promise.reject(error);
-    }
-  }
-)
+		if (typeof error.response === 'undefined') {
+			alert(
+				'A server/network error occurred. ' +
+					'Looks like CORS might be the problem. ' +
+					'Sorry about this - we will get it fixed shortly.'
+			);
+			return Promise.reject(error);
+		}
+
+		if (
+			error.response.status === 401 &&
+			originalRequest.url === baseURL + 'token/refresh/'
+		) {
+			window.location.href = '/login/';
+			return Promise.reject(error);
+		}
+
+		if (
+			error.response.data.code === 'token_not_valid' &&
+			error.response.status === 401 &&
+			error.response.statusText === 'Unauthorized'
+		) {
+			const refreshToken = localStorage.getItem('refresh_token');
+
+			if (refreshToken) {
+				const tokenParts = JSON.parse(atob(refreshToken.split('.')[1]));
+
+				// exp date in token is expressed in seconds, while now() returns milliseconds:
+				const now = Math.ceil(Date.now() / 1000);
+				console.log(tokenParts.exp);
+
+				if (tokenParts.exp > now) {
+					return axiosInstance
+						.post('/token/refresh/', { refresh: refreshToken })
+						.then((response) => {
+							localStorage.setItem('access_token', response.data.access);
+							localStorage.setItem('refresh_token', response.data.refresh);
+
+							axiosInstance.defaults.headers['Authorization'] =
+								'JWT ' + response.data.access;
+							originalRequest.headers['Authorization'] =
+								'JWT ' + response.data.access;
+
+							return axiosInstance(originalRequest);
+						})
+						.catch((err) => {
+							console.log(err);
+						});
+				} else {
+					console.log('Refresh token is expired', tokenParts.exp, now);
+					window.location.href = '/login/';
+				}
+			} else {
+				console.log('Refresh token not available.');
+				window.location.href = '/login/';
+			}
+		}
+
+		// specific error handling done elsewhere
+		return Promise.reject(error);
+	}
+);
 
 export default axiosInstance;
